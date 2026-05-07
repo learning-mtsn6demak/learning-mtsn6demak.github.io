2c452f2c09d7ddbf4bc74cbba5894f80cce8184b
hafalanqu <pptqattibyanlaren22gmail.com>
2026-05-07 07:38:06 +0700

    undo add export

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export', // Tambahkan baris ini
-  // pengaturan lain kalau ada...
+  /* config options here */
 };
 
 export default nextConfig;
+
